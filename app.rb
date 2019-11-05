ee48ecc29f979e9e087f8d370ab0a16ddea2437b
Jenny Chu <jchudesign0930@gmail.com>
2019-11-05 20:46:12 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,24 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do
+    
+    erb :user_input
+  end
+  
+  post '/piglatinize' do
+   user_word = params[:user_feedback]
+   @pig_latinized_word = Piglatinizer.new(params[:user_feedback])
+   
+   erb :pig_latin
+  end
+  
+  
+  
+  
+  
+  
+  
+  
 end
